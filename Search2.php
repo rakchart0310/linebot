668ec4bfdb5eb4288043c16563c4d87a2dc92364
rakchart0310 <37840220+rakchart0310@users.noreply.github.com>
2019-04-29 15:59:31 +0700

    Update Search2.php

--- Search2.php
+++ Search2.php
@@ -1,6 +1,6 @@
 <?php
     require('Connect_DB.php');
-    $sql_text = "SELECT * FROM Linebot1 WHERE Keyword LIKE '%a%'";
+    $sql_text = "SELECT * FROM newtable WHERE Keyword LIKE '%a%'";
     $query = mysqli_query($conn,$sql_text);
     while($objresult = mysqli_fetch_assoc($query))
     {
