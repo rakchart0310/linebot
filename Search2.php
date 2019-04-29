71d38878bf967c4c1caa820d60fb07a3b51dfb13
rakchart0310 <37840220+rakchart0310@users.noreply.github.com>
2019-04-29 16:24:33 +0700

    Update Search2.php

--- Search2.php
+++ Search2.php
@@ -4,6 +4,6 @@
     $query = mysqli_query($conn,$sql_text);
     while($objresult = mysqli_fetch_assoc($query))
     {
-        echo $objresult['type']."<br>";
+        echo $objresult['กฟฟ.']."<br>";
     }
 ?>
