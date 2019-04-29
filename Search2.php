fd3cbd40176c987dc1c95af97ac87495111e5686
rakchart0310 <37840220+rakchart0310@users.noreply.github.com>
2019-04-29 16:21:52 +0700

    Update Search2.php

--- Search2.php
+++ Search2.php
@@ -1,6 +1,6 @@
 <?php
     require('Connect_DB.php');
-    $sql_text = "SELECT * from dwdm where type = 'ILA';";
+    $sql_text = "SELECT * from dwdm where type = 'ILA'";
     $query = mysqli_query($conn,$sql_text);
     while($objresult = mysqli_fetch_assoc($query))
     {
