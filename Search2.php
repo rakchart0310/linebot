767f22b3e4c21ebe095d474b7956dc668db6a798
rakchart0310 <37840220+rakchart0310@users.noreply.github.com>
2019-04-29 16:06:24 +0700

    Update Search2.php

--- Search2.php
+++ Search2.php
@@ -1,6 +1,6 @@
 <?php
     require('Connect_DB.php');
-    $sql_text = "SELECT * FROM newtable WHERE Keyword LIKE '%a%'";
+    $sql_text = "SELECT * from newtable where id = '1'";
     $query = mysqli_query($conn,$sql_text);
     while($objresult = mysqli_fetch_assoc($query))
     {
