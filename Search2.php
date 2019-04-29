40245eebe3d4ca96bc03d9a4480d3daa636a9409
rakchart0310 <37840220+rakchart0310@users.noreply.github.com>
2019-04-29 16:08:33 +0700

    Update Search2.php

--- Search2.php
+++ Search2.php
@@ -4,6 +4,6 @@
     $query = mysqli_query($conn,$sql_text);
     while($objresult = mysqli_fetch_assoc($query))
     {
-        echo $objresult['Answer']."<br>";
+        echo $objresult['type']."<br>";
     }
 ?>
