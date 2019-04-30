ed675454c46a1f3193bb1ad58a82e29816d0aba5
rakchart0310 <37840220+rakchart0310@users.noreply.github.com>
2019-04-30 14:56:00 +0700

    ดด

--- liff_search.php
+++ liff_search.php
@@ -8,7 +8,7 @@
 		<script src="assets/js/jquery.min.js"></script>
 		<script src="assets/js/popper.min.js"></script>
 		<script src="assets/js/bootstrap.min.js"></script>
-		<script src="liff_search.js"></script>
+		<script src="/liff_search.js"></script>
 		<link href="https://fonts.googleapis.com/css?family=Sarabun|Roboto" rel="stylesheet">
 		
 	</head>
