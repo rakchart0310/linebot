e035a4507f62cb231e3492aaf3ac18805242608e
rakchart0310 <37840220+rakchart0310@users.noreply.github.com>
2019-04-30 16:11:30 +0700

    ff

--- liff_search.js
+++ liff_search.js
@@ -3,9 +3,14 @@ function test()
     var office = document.getElementById('office_select').value;
     alert(office);
 }
+function render_card(equip_name)
+{
+    var card = document.getElementById('card-area')
+    card.innerHTML = card.innerHTML + '<div class="col-lg-12"><div class="card mt-2"><div class="card-body">'+equip_name+'</div></div></div>';
+}
 function search()
 {
-    var office_select = document.getElementById('office_select').value
+    var office_select = document.getElementById("office_select").value
     var keyword = document.getElementById("keyword").value
     var formData = new FormData();
     formData.append('office', office_select);
@@ -19,8 +24,17 @@ function search()
 			processData: false,
 			contentType: false,
 			success: function(response) {
-                        console.log(response);
-                        var obj = JSON.parse(response);  
+                        //console.log(response);
+                        var card = document.getElementById('card-area')
+                        card.innerHTML = '';
+                        var obj = JSON.parse(response);
+                        var i = 0;
+						while(obj[i])
+						{
+                            render_card(obj[i].equip_name);
+                            console.log(obj[i].equip_name);
+							i++;
+						}  
                     }				
 			});
 }
